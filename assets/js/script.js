09c7317ecf6687d3b04d0e522be033257f46e82b
engrnonny <engr.nonny@gmail.com>
2022-09-06 08:50:38 +0100

    Added bootstrap popover to subset skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,3 +169,13 @@ $(document).ready(function(){
   });
 
 });
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const popover = new bootstrap.Popover('.popover-dismiss', {
+  trigger: 'focus'
+})
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
